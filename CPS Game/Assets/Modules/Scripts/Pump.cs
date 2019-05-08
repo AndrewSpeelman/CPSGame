a40062e7f59fe3c4ff422b8844a9669065fd0caa
Michael Rouse <mwrouse@outlook.com>
2019-05-08 10:07:28 -0500

    Fixed flow of water, need to change when it flows. Added solution for debugging code.

--- CPS Game/Assets/Modules/Scripts/Pump.cs
+++ CPS Game/Assets/Modules/Scripts/Pump.cs
@@ -60,7 +60,7 @@ namespace Assets.Modules.Scripts
             switch (AttackMenuOption)
             {
                 case Strings.AttackStrings.Pump.Flow:
-                    this.IsPumping = true; 
+                    this.IsPumping = false; 
                     break;
 
                 case Strings.AttackStrings.Pump.Sensor:
