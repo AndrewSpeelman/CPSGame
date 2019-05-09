eca9a1fb315a63f89773ffee3e817eb3431aa0d3
Wes Huhman <westonhuhman@gmail.com>
2019-05-08 21:44:26 -0500

    ScoreController Added

---
+++ CPS Game/Assets/GameLogic/UI/ScoreController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreController : MonoBehaviour
+{
+    private int attackerScore;
+    private int defenderScore;
+    public Text attackerScoreText;
+    public Text defenderScoreText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        attackerScore = 0;
+        defenderScore = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        attackerScoreText.text = "" + attackerScore;
+        defenderScoreText.text = "" + defenderScore;
+    }
+
+
+}
